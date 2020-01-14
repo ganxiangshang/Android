8c36491d7ce261c9cac736b22c77344da80cd17e
hong <zhs1012677197@163.com>
2020-01-15 00:39:30 +0800

    findId

--- app/src/main/java/com/example/android/MainActivity.java
+++ app/src/main/java/com/example/android/MainActivity.java
@@ -3,12 +3,21 @@ package com.example.android;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private View text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    private void initView(){
+        text = findViewById(R.id.text);
+    }
+
+
 }
