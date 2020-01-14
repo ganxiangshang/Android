49ddf9a3a3886fa6015972f7e583cf104c14151c
hong <zhs1012677197@163.com>
2020-01-15 00:35:31 +0800

    修改xml

--- app/src/main/java/com/example/android/MainActivity.java
+++ app/src/main/java/com/example/android/MainActivity.java
@@ -3,12 +3,20 @@ package com.example.android;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView text_android;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    private void initView(){
+        text_android = ((TextView) findViewById(R.id.test_android));
+    }
+
 }
