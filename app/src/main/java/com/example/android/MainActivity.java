0b727313fddce567cf58ef75f7631e5f652cf297
hong <zhs1012677197@163.com>
2020-01-15 00:44:45 +0800

    findId_text_master

--- app/src/main/java/com/example/android/MainActivity.java
+++ app/src/main/java/com/example/android/MainActivity.java
@@ -3,12 +3,24 @@ package com.example.android;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView text_master;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
+    private void initView(){
+        text_master = ((TextView) findViewById(R.id.text_master));
+
+    }
+
 }
