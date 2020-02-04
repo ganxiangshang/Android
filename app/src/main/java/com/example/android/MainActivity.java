cc9e649b55ebe5700c791ceb09003ed3770132bc
hong <zhs1012677197@163.com>
2020-02-04 21:32:38 +0800

    初始化View以及点击事件

--- app/src/main/java/com/example/android/MainActivity.java
+++ app/src/main/java/com/example/android/MainActivity.java
@@ -15,6 +15,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initView();
+        initEvent();
     }
 
     private void initView(){
@@ -35,6 +38,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
      * 点击事件
      */
     private void initEvent(){
+
+        text.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "点击事件", Toast.LENGTH_SHORT).show();
+            }
+        });
+
 
     }
 
