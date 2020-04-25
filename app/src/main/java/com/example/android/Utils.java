f6ee5b2f365bb2da4ec3268f441e3378bce2dbe8
zhs <zhs1012677197@163.com>
2020-04-25 18:50:33 +0800

    util

---
+++ app/src/main/java/com/example/android/Utils.java
@@ -0,0 +1,14 @@
+package com.example.android;
+
+public class Utils {
+
+
+    public static String getString(){
+
+        return "";
+    }
+
+
+
+
+}
---
+++ customeeventbus/build/generated/source/buildConfig/freeChina/debug/com/example/customeeventbus/BuildConfig.java
@@ -0,0 +1,15 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.customeeventbus;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.example.customeeventbus";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "freeChina";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+  public static final String FLAVOR_price = "free";
+  public static final String FLAVOR_nation = "china";
+}
