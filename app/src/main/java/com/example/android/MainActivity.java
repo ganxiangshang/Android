4c81a8e5e5771dfb16b0df51479d27bc1ca0e603
zhs <zhs1012677197@163.com>
2020-04-26 23:39:12 +0800

    delete unUse

--- app/src/main/java/com/example/android/MainActivity.java
+++ app/src/main/java/com/example/android/MainActivity.java
@@ -23,66 +23,27 @@ import java.util.HashMap;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ThreadPoolExecutor;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity {
 
     private View text;
 
-    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        getApplicationContext();
-
-        new Handler(new Handler.Callback() {
-            @Override
-            public boolean handleMessage(@NonNull Message msg) {
-                return false;
-            }
-        });
 
 
-        ArrayMap<String, String> stringStringArrayMap = new ArrayMap<>();
-
-//        new androidx.collection.ArrayMap<>()
 
 
         initView();
-        initEvent();
-
-        Handler handler = new Handler();
-
-        HandlerThread hong = new HandlerThread("hong");
-        Looper looper = hong.getLooper();
-        hong.start();
-
-        Looper looper1 = looper.myLooper();
-        Looper mainLooper1 = Looper.getMainLooper();
-
-        new Thread(){
-
-            @Override
-            public void run() {
-                super.run();
-                if (Looper.getMainLooper() == Looper.myLooper()){
-                    System.out.println("=============run===");
-                }else {
-                    System.out.println("=============no===");
-                }
-            }
-        }.start();
 
 
-        HashMap<String, String> stringStringHashMap = new HashMap<>();
-
-        //Glide.with(this).load("").into(new ImageView(this));
 
     }
 
     private void initView(){
         text = findViewById(R.id.text);
-        text.setOnClickListener(this);
         text.setVisibility(View.GONE);
         text.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -93,52 +54,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         text.setBackgroundColor(Color.RED);
 
-
-
     }
 
-    /**
-     * 点击事件
-     */
-    private void initEvent(){
 
-        text.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "点击事件", Toast.LENGTH_SHORT).show();
-            }
-        });
-    }
-
-    /**
-     * 我是注释
-     * @param v
-     */
-
-    @Override
-    public void onClick(View v) {
-
-    }
-
-    /**
-     * 初始化数据
-     */
-    private void initData(){
-
-        // 我是dev数据
-        ArrayList<String> strings = new ArrayList<>();
-        for (int i = 0; i < 10; i++){
-            strings.add(i + "我是master数据");
-        }
-
-        // 我是dev数 据 新加入
-        ArrayList<String> stringArrayList = new ArrayList<>();
-        for (int i = 0; i < 10; i++){
-            stringArrayList.add(i + "我是master数据");
-        }
-
-        //查看冲突
-
-    }
 
 }
