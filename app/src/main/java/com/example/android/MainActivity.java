4c3d593846e035a8011e1fc034f3216d0c21aad4
zhs <zhs1012677197@163.com>
2020-05-21 22:56:30 +0800

    修改444

--- app/src/main/java/com/example/android/MainActivity.java
+++ app/src/main/java/com/example/android/MainActivity.java
@@ -17,8 +17,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        String a = "aaaa_222";
-        String b = "bbbb_222";
+        String a = "aaaa_222_444";
+        String b = "bbbb_222_444";
 
 
         initView();
